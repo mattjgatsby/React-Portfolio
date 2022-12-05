d5b745ec79569fc65961ac3f6e4fa091cc4deff9
Gatsby <111303029+mattjgatsby@users.noreply.github.com>
2022-12-05 18:50:37 -0500

    added aboutme.js and home.js

---
+++ matt-gatsby-portfolio/src/components/home.js
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+import Header from './header';
+import Footer from "./footer";
+
+
+function Home() {
+    const renderPage = () => {
+
+    }
+}
+
+export default Home;
