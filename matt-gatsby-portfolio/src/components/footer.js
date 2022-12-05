b977b312b828ee1af0416a0bc7f8ef2aefa2b048
Gatsby <111303029+mattjgatsby@users.noreply.github.com>
2022-12-04 22:39:41 -0500

    modifying footer.js and added assets/images folder with Github Icon image

--- matt-gatsby-portfolio/src/components/footer.js
+++ matt-gatsby-portfolio/src/components/footer.js
@@ -3,6 +3,13 @@ import React from "react";
 const Footer = () => {
     return (
         <div>
+            <img
+            src="../../assets/images/GitHub-Mark-Light-64px.png"
+            alt="GitHub Icon"
+            href='https://github.com/mattjgatsby'
+            ></img>
         </div>
     )
 }
+
+export default Footer;
