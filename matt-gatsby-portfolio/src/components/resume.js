eb5867df5c8757a67253246b245320890eabbcfc
Gatsby <111303029+mattjgatsby@users.noreply.github.com>
2022-12-05 23:04:16 -0500

    added resume.png and wrote out jsx for resume.js

--- matt-gatsby-portfolio/src/components/resume.js
+++ matt-gatsby-portfolio/src/components/resume.js
@@ -1,11 +1,16 @@
 import React from "react";
+import myResume from "../assets/images/Resume.png";
 
 function Resume() {
-    return (
-        <div>
-
-        </div>
-    )
+  return (
+    <div className="card-body">
+      <h5 className="image_center h1">Click to Download!</h5>
+      <a href={myResume}
+      className='card-body'>
+        <img src={myResume} className="image_center h1" alt="my resume" />
+      </a>
+    </div>
+  );
 }
 
 export default Resume;
