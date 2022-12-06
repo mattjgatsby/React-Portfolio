90428a378655d950981f57a7464d8b14010876de
Gatsby <111303029+mattjgatsby@users.noreply.github.com>
2022-12-06 00:48:58 -0500

    adjusted some css on about page

--- matt-gatsby-portfolio/src/components/aboutMe.js
+++ matt-gatsby-portfolio/src/components/aboutMe.js
@@ -4,7 +4,7 @@ import mePicture from "../assets/images/Me2019.png";
 function About() {
   return (
     <div className="background ">
-      <h1 className="text-center ">Matthew Gatsby</h1>
+      <h1 className="text-center header_font_size">Matthew Gatsby</h1>
       <div className="row d-flex justify-content-center">
         <div className="col-sm-4">
           <img
@@ -13,11 +13,11 @@ function About() {
             alt="me from 2019"
           ></img>
         </div>
-        <div className="col-sm-5">
+        <div className="col-sm-5 custom_about_image">
           <p className="text-right m-4 custom_font_size">
             Passion for code in my humble abode! 28 year old, living in Northern
             Virginia. If I'm not at my computer I'm spending time with my three
-            dogs or rounding up the frogs.
+            dogs or rounding up the frogs. I'm dedicated to learning and growing my abilities as a web developer!
           </p>
         </div>
       </div>
