25dd373507ba8fd84e7ea5e71ea44b9ecbac907a
Gatsby <111303029+mattjgatsby@users.noreply.github.com>
2022-12-05 21:11:43 -0500

    added project gif and added project to project.js jsx

--- matt-gatsby-portfolio/src/components/projects/project.js
+++ matt-gatsby-portfolio/src/components/projects/project.js
@@ -1,12 +1,27 @@
 import React from "react";
 import SoundsLike from "../../assets/images/Sounds-like-Project.jpg";
+import WorkDay from '../../assets/images/Work Day Scheduler.gif'
 
 function Projects() {
   return (
-    <div class="card card_size">
-      <img src={SoundsLike} class="card-img-top" alt="Sounds Like Project ScreenShot" />
-      <div class="card-body">
+    <div>
+      <div class="card card_size">
+        <img
+          src={SoundsLike}
+          class="card-img-top"
+          alt="Sounds Like Project ScreenShot"
+        />
+        <div class="card-body"></div>
       </div>
+      <div class="card card_size">
+        <img
+          src={WorkDay}
+          class="card-img-top"
+          alt="Work Day Scheduler Gif"
+        />
+        <div class="card-body"></div>
+      </div>
+
     </div>
   );
 }
